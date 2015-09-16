25ef84c40c93fd2220e14277842ba3d095dc4e7f
MaxKlaxxMiner <max@tetrisk.de>
2015-09-16 20:57:39 +0200

    Unit-Test Projekt hinzugefügt

---
+++ SokoWahnCore.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿#region # using *.*
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+#endregion
+
+namespace SokoWahnCore.Test
+{
+  [TestClass]
+  public sealed class UnitTest1
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+  }
+}
