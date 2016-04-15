ae4b19556305ad380337e2b6299ebaf04eed9397
MaxKlaxx <max@tetrisk.de>
2016-04-15 03:44:37 +0200

    einfache Methoden zum erstellen von VisualStudio-Projekten hinzugefügt

--- SokoWahn/CsFile.cs
+++ SokoWahn/CsFile.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 // ReSharper disable UnusedMember.Global
+// ReSharper disable MemberCanBePrivate.Global
 
 #endregion
 
@@ -35,6 +36,10 @@ namespace SokoWahn
     /// </summary>
     int indentPos;
 
+    /// <summary>
+    /// merkt sich, ob bei der nächsten Ausgabe kein Einschub gemacht werden soll
+    /// </summary>
+    bool indentSkip;
 
     /// <summary>
     /// Konstruktor
@@ -47,10 +52,30 @@ namespace SokoWahn
       simplify = simplifyQuote;
       indentSize = indentTabSize;
       indentPos = 0;
+      indentSkip = false;
     }
 
     /// <summary>
-    /// schreibt eine Zeile mit optionale, weiteren Block in die Datei
+    /// Rückt die nachfolgenden Zeilen zusätzlich ein
+    /// </summary>
+    /// <param name="count">optional, Anzahl der Schritte (default: 1)</param>
+    public void Indent(int count = 1)
+    {
+      indentPos += indentSize * count;
+    }
+
+    /// <summary>
+    /// Rückt die nachfolgenden Zeilen wieder zurück
+    /// </summary>
+    /// <param name="count">optional, Anzahl der Schritte (default: 1)</param>
+    public void Dedent(int count = 1)
+    {
+      indentPos -= indentSize * count;
+    }
+
+    #region # public void Write(...)
+    /// <summary>
+    /// schreibt eine Zeile mit optionalen weiteren Block in die Datei
     /// </summary>
     /// <param name="line">Zeile, welche geschrieben werden soll</param>
     /// <param name="block">optionale Block-Methode für weitere Zeilen in einem {}-Block</param>
@@ -95,6 +120,87 @@ namespace SokoWahn
       {
         Write(line);
       }
+    }
+    #endregion
+
+    #region # public void WriteX(...)
+    /// <summary>
+    /// schreibt eine Xml-Zeile mit optionalen weiteren Block in die Datei
+    /// </summary>
+    /// <param name="line">Zeile, welche geschrieben werden soll</param>
+    /// <param name="block">optionale Block-Methode für weitere Zeilen in einem {}-Block</param>
+    public void WriteX(string line, Action<CsFile> block = null)
+    {
+      if (simplify) line = line.Replace('\'', '\"').Replace('´', '\'');
+
+      if (!string.IsNullOrWhiteSpace(line))
+      {
+        if (!indentSkip) data.Append(' ', indentPos); else indentSkip = false;
+        data.Append('<').Append(line);
+        if (block == null) data.Append(" />"); else data.Append(">");
+      }
+      data.AppendLine();
+
+      if (block == null) return;
+
+      string xmlName = line;
+      int p = xmlName.IndexOf(' ');
+      if (p > 0) xmlName = xmlName.Substring(0, p);
+
+      indentPos += indentSize;
+      block(this);
+      indentPos -= indentSize;
+
+      if (!indentSkip) data.Append(' ', indentPos); else indentSkip = false;
+      data.Append("</").Append(xmlName).Append('>').AppendLine();
+    }
+
+    /// <summary>
+    /// schreibt eine oder mehrere Xml-Zeilen in die Datei (leer = Leerzeile)
+    /// </summary>
+    /// <param name="lines">Zeilen, welche geschrieben werden sollen</param>
+    public void WriteX(params string[] lines)
+    {
+      if (lines.Length == 0) Write("");
+      foreach (var line in lines)
+      {
+        Write(line);
+      }
+    }
+
+    /// <summary>
+    /// schreibt mehrere Xml-Zeilen in die Datei
+    /// </summary>
+    /// <param name="lines">Enumerable der Zeilen, welche geschrieben werden sollen</param>
+    public void WriteX(IEnumerable<string> lines)
+    {
+      foreach (var line in lines)
+      {
+        Write(line);
+      }
+    }
+    #endregion
+
+    /// <summary>
+    /// schreibt etwas ohne Zeilenumbruch in die Datei
+    /// </summary>
+    /// <param name="txt">Text, welcher geschrieben werden soll</param>
+    public void WriteV(string txt)
+    {
+      if (data.Length > 1 && data[data.Length - 2] == '\r' && data[data.Length - 1] == '\n') data.Remove(data.Length - 2, 2);
+      if (simplify) txt = txt.Replace('\'', '\"').Replace('´', '\'');
+      data.Append(txt);
+      indentSkip = true;
+    }
+
+    /// <summary>
+    /// schreibt ein einfachen Xml-Wert
+    /// </summary>
+    /// <param name="xmlTag">Xml-Tag mit Attributen</param>
+    /// <param name="value">Wert, welcher geschrieben werden soll</param>
+    public void WriteXv(string xmlTag, string value)
+    {
+      WriteX(xmlTag, f => f.WriteV(value));
     }
 
     /// <summary>
--- SokoWahn/Program.cs
+++ SokoWahn/Program.cs
@@ -31,17 +31,146 @@ namespace SokoWahn
                            + "####  ###  ####\n"
                            + "   #### ####   \n";
 
+    #region # static CsFile CreateCsProjectFile(Guid projectGuid, string projectName, IEnumerable<string> references, IEnumerable<string> compileFiles) // erstellt eine einfache Visual Studio Projekt-Datei (Konsolenzeilen Programm)
+    /// <summary>
+    /// erstellt eine einfache Visual Studio Projekt-Datei (Konsolenzeilen Programm)
+    /// </summary>
+    /// <param name="projectGuid">GUID des Projektes</param>
+    /// <param name="projectName">Name des Projektes (Assembly-Name)</param>
+    /// <param name="references">Liste mit erforderlichen Verweisen</param>
+    /// <param name="compileFiles">Liste der compilierbaren Dateien, welche zum Projekt gehören</param>
+    /// <returns>fertig erstellte Projekt-Datei</returns>
+    static CsFile CreateCsProjectFile(Guid projectGuid, string projectName, IEnumerable<string> references, IEnumerable<string> compileFiles)
+    {
+      var prj = new CsFile(true);
+
+      prj.Write("<?xml version='1.0' encoding='utf-8'?>");
+      prj.WriteX("Project ToolsVersion='12.0' DefaultTargets='Build' xmlns='http://schemas.microsoft.com/developer/msbuild/2003'", p =>
+      {
+        p.WriteX("Import Project='$(MSBuildExtensionsPath)\\$(MSBuildToolsVersion)\\Microsoft.Common.props' Condition='Exists(´$(MSBuildExtensionsPath)\\$(MSBuildToolsVersion)\\Microsoft.Common.props´)'");
+        p.WriteX("PropertyGroup", x =>
+        {
+          x.WriteXv("Configuration Condition=' ´$(Configuration)´ == ´´ '", "Debug");
+          x.WriteXv("Platform Condition=' ´$(Platform)´ == ´´ '", "AnyCPU");
+          x.WriteXv("ProjectGuid", "{" + projectGuid.ToString().ToUpperInvariant() + "}");
+          x.WriteXv("OutputType", "Exe");
+          x.WriteXv("AppDesignerFolder", "Properties");
+          x.WriteXv("RootNamespace", projectName);
+          x.WriteXv("AssemblyName", projectName);
+          x.WriteXv("TargetFrameworkVersion", "v4.5");
+          x.WriteXv("FileAlignment", "512");
+        });
+        p.WriteX("PropertyGroup Condition=' ´$(Configuration)|$(Platform)´ == ´Debug|AnyCPU´ '", x =>
+        {
+          x.WriteXv("PlatformTarget", "AnyCPU");
+          x.WriteXv("DebugSymbols", "true");
+          x.WriteXv("DebugType", "full");
+          x.WriteXv("Optimize", "false");
+          x.WriteXv("OutputPath", "bin\\Debug\\");
+          x.WriteXv("DefineConstants", "DEBUG;TRACE");
+          x.WriteXv("ErrorReport", "prompt");
+          x.WriteXv("WarningLevel", "4");
+          x.WriteXv("Prefer32Bit", "false");
+          x.WriteXv("AllowUnsafeBlocks", "true");
+        });
+        p.WriteX("PropertyGroup Condition=' ´$(Configuration)|$(Platform)´ == ´Release|AnyCPU´ '", x =>
+        {
+          x.WriteXv("PlatformTarget", "AnyCPU");
+          x.WriteXv("DebugType", "pdbonly");
+          x.WriteXv("Optimize", "true");
+          x.WriteXv("OutputPath", "bin\\Release\\");
+          x.WriteXv("DefineConstants", "TRACE");
+          x.WriteXv("ErrorReport", "prompt");
+          x.WriteXv("WarningLevel", "4");
+          x.WriteXv("Prefer32Bit", "false");
+          x.WriteXv("AllowUnsafeBlocks", "true");
+        });
+        p.WriteX("ItemGroup", x =>
+        {
+          foreach (var r in references)
+          {
+            x.WriteX("Reference Include='" + r + "'");
+          }
+        });
+        p.WriteX("ItemGroup", x =>
+        {
+          foreach (var file in compileFiles)
+          {
+            x.WriteX("Compile Include='" + file + "'");
+          }
+        });
+        p.WriteX("Import Project='$(MSBuildToolsPath)\\Microsoft.CSharp.targets'");
+      });
+
+      return prj;
+    }
+    #endregion
+
+    #region # static CsFile CreateSolutionFile(Guid solutionGuid, Guid projectGuid, string projectName, string projectFile) // erstellt eine einfach Projektmappen-Datei
+    /// <summary>
+    /// erstellt eine einfach Projektmappen-Datei
+    /// </summary>
+    /// <param name="solutionGuid">GUID der Projektmappe</param>
+    /// <param name="projectGuid">GUID des Projektes</param>
+    /// <param name="projectName">Name des Projektes</param>
+    /// <param name="projectFile">Dateiname des Projektes</param>
+    /// <returns>fertig erstellte Projektmappen-Datei</returns>
+    static CsFile CreateSolutionFile(Guid solutionGuid, Guid projectGuid, string projectName, string projectFile)
+    {
+      var sol = new CsFile(true);
+
+      sol.Write("Microsoft Visual Studio Solution File, Format Version 12.00");
+      sol.Write("# Visual Studio 2013");
+      sol.Write("VisualStudioVersion = 12.0.31101.0");
+      sol.Write("MinimumVisualStudioVersion = 10.0.40219.1");
+      sol.Write("Project('{" + solutionGuid.ToString().ToUpperInvariant() + "}') = '" + projectName + "', '" + projectFile + "', '{" + projectGuid.ToString().ToUpperInvariant() + "}'");
+      sol.Write("EndProject");
+      sol.Write("Global");
+      {
+        sol.Write("\tGlobalSection(SolutionConfigurationPlatforms) = preSolution");
+        {
+          sol.Write("\t\tDebug|Any CPU = Debug|Any CPU");
+          sol.Write("\t\tRelease|Any CPU = Release|Any CPU");
+        }
+        sol.Write("\tEndGlobalSection");
+        sol.Write("\tGlobalSection(ProjectConfigurationPlatforms) = postSolution");
+        {
+          sol.Write("\t\t{" + projectGuid.ToString().ToUpperInvariant() + "}.Debug|Any CPU.ActiveCfg = Debug|Any CPU");
+          sol.Write("\t\t{" + projectGuid.ToString().ToUpperInvariant() + "}.Debug|Any CPU.Build.0 = Debug|Any CPU");
+          sol.Write("\t\t{" + projectGuid.ToString().ToUpperInvariant() + "}.Release|Any CPU.ActiveCfg = Release|Any CPU");
+          sol.Write("\t\t{" + projectGuid.ToString().ToUpperInvariant() + "}.Release|Any CPU.Build.0 = Release|Any CPU");
+        }
+        sol.Write("\tEndGlobalSection");
+        sol.Write("\tGlobalSection(SolutionProperties) = preSolution");
+        {
+          sol.Write("\t\tHideSolutionNode = FALSE");
+        }
+        sol.Write("\tEndGlobalSection");
+      }
+      sol.Write("EndGlobal");
+
+      return sol;
+    }
+    #endregion
+
     static void Main(string[] args)
     {
       Directory.CreateDirectory(PathData);
       Directory.CreateDirectory(PathTest);
 
-      var csFile = new CsFile(true);
+      var solutionGuid = Guid.NewGuid();
+      var projectGuid = Guid.NewGuid();
+      const string ProjectName = "Sokowahn";
+      const string ProjectFile = ProjectName + ".csproj";
+      const string SolutionFile = ProjectName + ".sln";
+      const string CsFile = ProjectName + ".cs";
 
+
+      var csFile = new CsFile(true);
       csFile.Write();
       csFile.Write("using System;");
       csFile.Write();
-      csFile.Write("namespace", ns =>
+      csFile.Write("namespace " + ProjectName, ns =>
       {
         ns.Write("unsafe class Program", cl =>
         {
@@ -53,9 +182,13 @@ namespace SokoWahn
         });
       });
 
-      csFile.SaveToFile(PathTest + "test.cs");
-      Console.WriteLine(new FileInfo(PathTest + "test.cs").FullName);
-      Console.ReadLine();
+      csFile.SaveToFile(PathTest + CsFile);
+
+      var projectFile = CreateCsProjectFile(projectGuid, ProjectName, new[] { "System" }, new[] { CsFile });
+      projectFile.SaveToFile(PathTest + ProjectFile);
+
+      var solutionFile = CreateSolutionFile(solutionGuid, projectGuid, "Sokowahn", ProjectFile);
+      solutionFile.SaveToFile(PathTest + SolutionFile);
     }
   }
 }
