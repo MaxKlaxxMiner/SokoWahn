56e836a0bd75a0acd055479d4a929e81f07f8aff
MaxKlaxxMiner <max@tetrisk.de>
2016-07-14 18:56:10 +0200

    minigame ausgelagert

---
+++ SokoWahn/MiniGame.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SokoWahnCore;
+
+namespace SokoWahn
+{
+  /// <summary>
+  /// Mini Konsolen-Spiel
+  /// </summary>
+  public static class MiniGame
+  {
+    /// <summary>
+    /// startet ein Mini-Konsolen Spiel
+    /// </summary>
+    /// <param name="field">Spielfeld, was gespielt werden soll</param>
+    public static void Run(SokowahnField field)
+    {
+      var game = new SokowahnField(field);
+      var steps = new Stack<ushort[]>();
+
+      for (; ; )
+      {
+        string output = game.ToString();
+        int playerChar = output.IndexOfAny(new[] { '@', '+' });
+
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.Write(output.Substring(0, playerChar));
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write(output[playerChar]);
+
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.Write(output.Remove(0, playerChar + 1));
+
+        Console.WriteLine(); Console.WriteLine();
+        Console.WriteLine("Steps:  " + steps.Count.ToString("N0"));
+        Console.WriteLine();
+        Console.WriteLine("Remain: " + game.boxesRemain);
+
+        if (game.boxesRemain == 0) return;
+
+        bool step = false;
+        var oldState = game.GetGameState();
+
+        switch (Console.ReadKey(true).Key)
+        {
+          case ConsoleKey.Escape: return;
+
+          case ConsoleKey.A:
+          case ConsoleKey.NumPad4:
+          case ConsoleKey.LeftArrow: step = game.MoveLeft(); break;
+
+          case ConsoleKey.D:
+          case ConsoleKey.NumPad6:
+          case ConsoleKey.RightArrow: step = game.MoveRight(); break;
+
+          case ConsoleKey.W:
+          case ConsoleKey.NumPad8:
+          case ConsoleKey.UpArrow: step = game.MoveUp(); break;
+
+          case ConsoleKey.S:
+          case ConsoleKey.NumPad2:
+          case ConsoleKey.DownArrow: step = game.MoveDown(); break;
+
+          case ConsoleKey.Z:
+          case ConsoleKey.Delete:
+          case ConsoleKey.Backspace:
+          {
+            if (steps.Count == 0) break;
+            game.SetGameState(steps.Pop(), 0);
+          } break;
+
+          default: continue;
+        }
+
+        if (step)
+        {
+          steps.Push(oldState);
+        }
+      }
+    }
+  }
+}
---
+++ SokoWahn/TestData.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SokoWahn
+{
+  public class TestData
+  {
+    /// <summary>
+    /// 78 moves, 17 pushes - LrRluulldlddrUUUUdddlllluurururRRlddrruUUdddrrdrddlUUUUdddrrrruulululLLrddlluU
+    /// </summary>
+    const string Level1 = "       ###  ^_^  \n"
+                        + "       #.#       \n"
+                        + "   #####.#####   \n"
+                        + "  ##         ##  \n"
+                        + " ##  # # # #  ## \n"
+                        + " #  ##     ##  # \n"
+                        + " # ##  # #  ## #     \n"
+                        + " #     $@$     # \n"
+                        + " ####  ###  #### \n"
+                        + "    #### #### :) \n";
+
+    /// <summary>
+    /// 115 moves, 20 pushes - dlllddRuurrddrrddllldlluRUUluurrrddrrddllLdlUrrrruullDuuulllddrRlluurrrdDllUluRRurDllddrrrrddlLLdlluRUUrrrrddllLdlU
+    /// </summary>
+    const string Level2 = "  ####\n"
+                        + "### @#\n"
+                        + "#    #\n"
+                        + "# .#.###\n"
+                        + "# $    #\n"
+                        + "##*#*# #\n"
+                        + "# $    #\n"
+                        + "#   ####\n"
+                        + "#####";
+
+    /// <summary>
+    /// 230 moves, 97 pushes - ullluuuLUllDlldddrRRRRRRRRRRurDllllllluuululldDDuulldddrRRRRRRRRRRdRRlUllllllluuulLulDDDuulldddrRRRRRRRRRRRRlllllllllllllulldRRRRRRRRRRRRRuRRlDllllllluuulluuurDDuullDDDDDuulldddrRRRRRRRRRRRllllllluuuLLulDDDuulldddrRRRRRRRRRRdRUluR
+    /// </summary>
+    const string Level3 = "    #####\n"
+                        + "    #   #\n"
+                        + "    #$  #\n"
+                        + "  ###  $##\n"
+                        + "  #  $ $ #\n"
+                        + "### # ## #   ######\n"
+                        + "#   # ## #####  ..#\n"
+                        + "# $  $          ..#\n"
+                        + "##### ### #@##  ..#\n"
+                        + "    #     #########\n"
+                        + "    #######";
+
+    /// <summary>
+    /// 206 moves, 59 pushes - uuuuuruulllddddddLdlUUUUUUddddrruuuuurrrddlLrddddLLLdlUUUUUdddrruuUrrruulllDDDDDuuurrddddlLLdlUUUUdddrrrrdrruurrdLruuuuLLLLLrruullldDDDDuuurrddddlLLdlUUUddrrrrdrrUUUUddrruuulLLLLrruullldDDDDuuurrddddlLLdlUU
+    /// </summary>
+    const string Level4 = "########\n"
+                        + "#.#    #\n"
+                        + "#.# ## ####\n"
+                        + "#.# $   $ #\n"
+                        + "#.# # # # #\n"
+                        + "#.# # # # #\n"
+                        + "#   # #   #\n"
+                        + "# $ $ # $ #\n"
+                        + "#   #@  ###\n"
+                        + "#########\n";
+
+    /// <summary>
+    /// 361 moves, 139 pushes - llllulldRRRRRRRuuuuluurDDDDDDldRRRRurDlllluuuuuurrrurrdLLLLulDDDDDDldRRRlluuuuuurrrdddrUUruLLLLulDDDDDDRRRDullllllllluuuuulllddRRlluurrrdDDDldRRRRRRdRRRRlluulDldRRRlluuuuuurrrrrddrrurrdLLLLdlUUruLLLLulDDDDDDldRRullllllluuulllddRRlluurrrdDldRRRRRRRRRDulllllllluuuuurrrddlUruLLruuullddDDDDDldRRRRRRRRluuuuurrrrrddrrrddddddrrdLdlUUUUUUUruLLLLdlUUruLLLLulDDDDDDldRR
+    /// </summary>
+    const string Level5 = "   #####\n"
+                        + "   #   #\n"
+                        + "   # # ##########\n"
+                        + "#### #  #   #   #\n"
+                        + "#       # $   $ #####\n"
+                        + "# ## #$ #   #   #   #\n"
+                        + "# $  #  ## ## $   $ #\n"
+                        + "# ## #####  #   #   #\n"
+                        + "# $   #  #  ###### ##\n"
+                        + "### $    @  ...# # #\n"
+                        + "  #   #     ...# # #\n"
+                        + "  #####  ###...# # ###\n"
+                        + "      #### ##### #   #\n"
+                        + "                 # $ #\n"
+                        + "                 #   #\n"
+                        + "                 #####\n";
+
+    /// <summary>
+    /// 1121 moves, 303 pushes - dllluullldddldldldldllURURURURUdldldldlluRuRuRuRuRRurrdLddLdLdLdLdlluuururururRlldldldldddrruLUlldRdrruruLrrururuUllldldDRURUdlluRuRRuRRRRddddddrrrdrruuuuuuuullllDDullldLddLLLuurRllddrrruUluRRRRddddddrrruuuulLrrdddddrruuuuuuuullllDDulllldddllUluRRlddrruUluRRRRdDRdLdddrrruuuuLLrrdddddrruuuuuuuullllDDullllldllddddrUUUluRRlddrruUluRRRRdDDDlddrrrruuuulLrrdddddrruuuuuuuullllDDullllldlldddddldlluuuRRRdrUUUluRRlddrruUluRRRRdDlDDrDLdRRRuruuulLdLdddrrdrrruuuuuuuullllDDullllldlldddlluRRdrUUluRRlddrruUluRRRRdDDulDDDrdLrrruruuulLdlDDldRddrrurrruuuuuuuullllDDullllldlldddddlUUluRRdrUUluRRlddrruUluRRRRdDDDDlddrdrruuuruuulLrrdddlddrrruuuuuuuullllDDulllldddllllldldRRdrUUldddlUUluRRRdrUUUluRRlddrruUluRRRRdDDlddddrdrruuuruuulLrrdddlddrrruuuuuuuullllDDullllldlldldRdrUUluRRlddrruUluRRRRdDldddddrdrruuuruuulLdLrurrdddlddrrruuuuuuuullllDldDrrrdddDldRRdrUUUUUUUUdddddddllldlluluurDRRurDldRRdrUUUUUUUddddddllldllUluRRRurDldRRdrUUUUUUdddddlllullldlluRRRRRurDldRRdrUUUUUddddlluuuuullluurDldlDDDulDDldRRluRRRRurDldRRdrUUUUdddllldllUluRRRurDldRRdrUUUddlluuuuulLdLDDldRRRurDldRRdrUUdlluuuuullulDDDDldRRRurDldRRdrUlluuuuullllDDDldRRRRurDldRR
+    /// </summary>
+    const string Level6 = "           #######\n"
+                        + "      ######    .#\n"
+                        + "    ###      ###.#\n"
+                        + "   ##     #  #@#.#\n"
+                        + "  ##  $# #     #.#\n"
+                        + " ##  $$  #   # #.#\n"
+                        + "##  $$  #   ## #.#\n"
+                        + "#  $$  ##   #  #.#\n"
+                        + "# $$  ##       #.#\n"
+                        + "##   ###    #   .#\n"
+                        + " ##### ####   #  #\n"
+                        + "          ########\n";
+
+    /// <summary>
+    /// 229 moves, 76 pushes - RRRUUluRRRdddLdlUUUluRllluurrDullddrRdrddrruuuuuluurDDDDDuuuurrrddlUrdddlULrUruuLLLulDrddDrrrdddLUUddLLLdlUUdlllUUUrruullDDDDldRRRRRRRlllllluuuuurruRRurDDurrrddlUruLLrrdddLLrrddddlUUUruLdddllllllluuuuurrurrurDlllddLulDDDDldRRRRRR
+    /// </summary>
+    const string Level7 = "     ####   \n"
+                        + "   ###  ####\n"
+                        + " ### $ $   #\n"
+                        + " #   #..#$ #\n"
+                        + " # $$#*.#  #\n"
+                        + " #   ....$ #\n"
+                        + "## # .#.#  #\n"
+                        + "# $##$#.#$ #\n"
+                        + "# @$    .$ #\n"
+                        + "##  #  ##  #\n"
+                        + " ###########\n";
+
+    /// <summary>
+    /// extrem
+    /// </summary>
+    const string Level8 = "####################\n"
+                        + "#                  #\n"
+                        + "# $  $ $ $ $ $ $ $ #\n"
+                        + "# $$$$$###########################################\n"
+                        + "#                         .                      #\n"
+                        + "# $$$$$#  $ $ $ $ $ ########################## # #\n"
+                        + "#      #  $ $ $ $ $ #   $  $  $  $  $  $  $    # #\n"
+                        + "# $$$$$#  ### # # ## #                       $   #\n"
+                        + "#      #  #        #  # ##################### ## #\n"
+                        + "# $$$$$#  #### ## ##$ #   #                 # #  #\n"
+                        + "#      #     # #  #   # # .                 # #  #\n"
+                        + "# $$$$$#  $$$# #  #   # # ################ ## #  #\n"
+                        + "#      #     # #  # $ # #                #  #$#  #\n"
+                        + "# $$$$$#  $$$# #  #   # # ############## #  # #  #\n"
+                        + "#      #     # #  #   # #.#............# #  # #  #\n"
+                        + "# $$$$$#  $$$# #  # $ # #.#............# #  # #  #\n"
+                        + "#      #     # #  #   # #.#............# #  #    #\n"
+                        + "# $$$$$#  $$$# #  #   # #.#............# #  #$#  #\n"
+                        + "#      #     # #  # $ # #.#............# #  # #  #\n"
+                        + "# $$$$$#  $$$# #  #   # #.#............# #  # #  #\n"
+                        + "#      #     # #  #   # #.#.........   # #  # #  #\n"
+                        + "#@$$$$$#  $$$# #  # $ #  ..............# #  #    #\n"
+                        + "#      #     # #  #   # #.#.........  .# #  #$#  #\n"
+                        + "# $$$$$#  $$$# #  #   # #.#............# #  # #  #\n"
+                        + "#      #     # #  # $ # #.#............# #  # #  #\n"
+                        + "# $$$$$#  $$$# #  #   # #.#............# #  # #  #\n"
+                        + "#      #     # #  #   # #.#............# #  #    #\n"
+                        + "# $$$$$#  $$$# #  # $ # #.#............# #  #$#  #\n"
+                        + "#      #     # #  #   # # #............# #  # #  #\n"
+                        + "# $$$$$#  $$$# #  #   # # ################  # #  #\n"
+                        + "#      #     # #  # # # #                #  # #  #\n"
+                        + "# $$$$$#  $$$# #  # $                       #    #\n"
+                        + "#      #     # #  ## # ###################$##$#  #\n"
+                        + "# $$$$$#  #    #     #                   # ## #  #\n"
+                        + "#      #  #### ##### #  $$ $$ $$ $$ $$ $$   # #  #\n"
+                        + "# $$$$$#           # # $  $  $  $  $  $  $$ # #  #\n"
+                        + "#      #  ###  # # # # $  $  $  $  $  $  $  $ $  #\n"
+                        + "# $$$$$######### # # ######################## ## #\n"
+                        + "#                #                               #\n"
+                        + "#      #       #                                 #\n"
+                        + "##################################################\n";
+  }
+}
