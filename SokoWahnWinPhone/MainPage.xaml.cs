fb26bae5029cd5e41659cb64000e4dc2247da007
MaxKlaxxMiner <max@tetrisk.de>
2016-07-16 01:46:00 +0200

    resharp

--- SokoWahnWinPhone/MainPage.xaml.cs
+++ SokoWahnWinPhone/MainPage.xaml.cs
@@ -17,32 +17,32 @@ using Windows.UI.Xaml.Navigation;
 
 namespace SokoWahnWinPhone
 {
-    /// <summary>
-    /// Eine leere Seite, die eigenständig verwendet werden kann oder auf die innerhalb eines Rahmens navigiert werden kann.
-    /// </summary>
-    public sealed partial class MainPage : Page
+  /// <summary>
+  /// Eine leere Seite, die eigenständig verwendet werden kann oder auf die innerhalb eines Rahmens navigiert werden kann.
+  /// </summary>
+  public sealed partial class MainPage
+  {
+    public MainPage()
     {
-        public MainPage()
-        {
-            this.InitializeComponent();
+      InitializeComponent();
 
-            this.NavigationCacheMode = NavigationCacheMode.Required;
-        }
-
-        /// <summary>
-        /// Wird aufgerufen, wenn diese Seite in einem Rahmen angezeigt werden soll.
-        /// </summary>
-        /// <param name="e">Ereignisdaten, die beschreiben, wie diese Seite erreicht wurde.
-        /// Dieser Parameter wird normalerweise zum Konfigurieren der Seite verwendet.</param>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            // TODO: Seite vorbereiten, um sie hier anzuzeigen.
-
-            // TODO: Wenn Ihre Anwendung mehrere Seiten enthält, stellen Sie sicher, dass
-            // die Hardware-Zurück-Taste behandelt wird, indem Sie das
-            // Windows.Phone.UI.Input.HardwareButtons.BackPressed-Ereignis registrieren.
-            // Wenn Sie den NavigationHelper verwenden, der bei einigen Vorlagen zur Verfügung steht,
-            // wird dieses Ereignis für Sie behandelt.
-        }
+      NavigationCacheMode = NavigationCacheMode.Required;
     }
+
+    /// <summary>
+    /// Wird aufgerufen, wenn diese Seite in einem Rahmen angezeigt werden soll.
+    /// </summary>
+    /// <param name="e">Ereignisdaten, die beschreiben, wie diese Seite erreicht wurde.
+    /// Dieser Parameter wird normalerweise zum Konfigurieren der Seite verwendet.</param>
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+      // TODO: Seite vorbereiten, um sie hier anzuzeigen.
+
+      // TODO: Wenn Ihre Anwendung mehrere Seiten enthält, stellen Sie sicher, dass
+      // die Hardware-Zurück-Taste behandelt wird, indem Sie das
+      // Windows.Phone.UI.Input.HardwareButtons.BackPressed-Ereignis registrieren.
+      // Wenn Sie den NavigationHelper verwenden, der bei einigen Vorlagen zur Verfügung steht,
+      // wird dieses Ereignis für Sie behandelt.
+    }
+  }
 }
