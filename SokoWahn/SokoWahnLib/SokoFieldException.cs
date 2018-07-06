b9da435d35adb27df1aef9ddd6f7ff8ec12dde1e
MaxKlaxxMiner <max@tetrisk.de>
2018-07-06 19:23:45 +0200

    Lib: SokoFieldException hinzugefügt

---
+++ SokoWahn/SokoWahnLib/SokoFieldException.cs
@@ -0,0 +1,14 @@
+﻿#region # using *.*
+using System;
+#endregion
+
+namespace SokoWahnLib
+{
+  /// <summary>
+  /// Exception, wenn ein Fehler im Spielfeld gefunden wurde
+  /// </summary>
+  public class SokoFieldException : Exception
+  {
+    public SokoFieldException(string message) : base(message) { }
+  }
+}
