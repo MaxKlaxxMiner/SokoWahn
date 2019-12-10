962907319b2148fba0aa36a23d257f6ddd79f500
MaxKlaxx <max@tetrisk.de>
2019-12-10 04:13:06 +0100

    Rooms: Windows-GUI begonnen für leichtere Benutzung (Dark-Style)

---
+++ SokoWahn/SokoWahnWin/FormDebugger.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace SokoWahnWin
+{
+  partial class FormDebugger
+  {
+    /// <summary>
+    /// Erforderliche Designervariable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Verwendete Ressourcen bereinigen.
+    /// </summary>
+    /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Vom Windows Form-Designer generierter Code
+
+    /// <summary>
+    /// Erforderliche Methode für die Designerunterstützung.
+    /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.button1 = new System.Windows.Forms.Button();
+      this.listRooms = new System.Windows.Forms.ListBox();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.button1.Location = new System.Drawing.Point(1054, 12);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(186, 36);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "Step";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form_KeyDown);
+      // 
+      // listRooms
+      // 
+      this.listRooms.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+      this.listRooms.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+      this.listRooms.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.listRooms.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(86)))), ((int)(((byte)(156)))), ((int)(((byte)(214)))));
+      this.listRooms.FormattingEnabled = true;
+      this.listRooms.ItemHeight = 15;
+      this.listRooms.Location = new System.Drawing.Point(1, 1);
+      this.listRooms.Name = "listRooms";
+      this.listRooms.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+      this.listRooms.Size = new System.Drawing.Size(132, 514);
+      this.listRooms.TabIndex = 1;
+      this.listRooms.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form_KeyDown);
+      // 
+      // FormDebugger
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.Color.Black;
+      this.ClientSize = new System.Drawing.Size(1252, 516);
+      this.Controls.Add(this.listRooms);
+      this.Controls.Add(this.button1);
+      this.Name = "FormDebugger";
+      this.Text = "Form1";
+      this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form_KeyDown);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.ListBox listRooms;
+  }
+}
+
---
+++ SokoWahn/SokoWahnWin/FormDebugger.cs
@@ -0,0 +1,222 @@
+﻿#region # using *.*
+// ReSharper disable RedundantUsingDirective
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SokoWahnLib;
+using SokoWahnLib.Rooms;
+// ReSharper disable FieldCanBeMadeReadOnly.Local
+// ReSharper disable UnusedMember.Local
+#endregion
+
+namespace SokoWahnWin
+{
+  public partial class FormDebugger : Form
+  {
+    RoomNetwork network;
+
+    #region # // --- normale einfache Spielfelder ---
+    static readonly SokoField FieldTest1 = new SokoField(@"
+      ######
+      #    #
+      # $@.#
+      ######
+    ");
+
+    static readonly SokoField FieldStart = new SokoField(@"
+          #####
+          #   #
+          #$  #
+        ###  $##
+        #  $ $ #
+      ### # ## #   ######
+      #   # ## #####  ..#
+      # $  $          ..#
+      ##### ### #@##  ..#
+          #     #########
+          #######
+    ");
+    #endregion
+
+    #region # // --- bisher unlösbare Spielfelder ---
+    static readonly SokoField Field628 = new SokoField(@"
+      ####################
+      #+.... #   #   #   #
+      #...   $   $   $   #
+      #...   #   #   #   #
+      #.   ############$##
+      #.  ##  #  #  ##   #
+      ##$##   $ $#$      #
+      #   #   #  $  ##   #
+      #   #####     ######
+      #   #   ###$###     
+      ##$## $ ##   #      
+       # ##   ##   #      
+       # #  ####   #      
+       #     #######      
+       # #     #          
+       # ##$## #          
+       #    #  #          
+       #### # ##          
+          #   #           
+          #####
+    ");
+
+    static readonly SokoField FieldMoves105022 = new SokoField(@"
+       #               ###  
+      #.###############   # 
+      # $                $# 
+      #*.**************.**@#
+      #                $ $ #
+       ################.  # 
+                       ###  
+    ");
+
+    static readonly SokoField FieldMonster = new SokoField(@"
+      ###################################
+      #..... $ $ $ $ $   $ $ $ $ $ .....#
+      #.....  $ $ $ $ $ $ $ $ $ $  .....#
+      #..... $ $ $ $ $   $ $ $ $ $ .....#
+      #.....  $ $ $ $ $ $ $ $ $ $  .....#
+      #..... $ $ $ $ $   $ $ $ $ $ .....#
+      #.....  $ $ $ $ $ $ $ $ $ $  .....#
+      #..... $ $ $ $ $   $ $ $ $ $ .....#
+      #.....  $ $ $ $ $ $ $ $ $ $  .....#
+      #..... $ $ $ $ $   $ $ $ $ $ .....#
+      #.....  $ $ $ $ $@$ $ $ $ $  .....#
+      #..... $ $ $ $ $   $ $ $ $ $ .....#
+      #.....  $ $ $ $ $ $ $ $ $ $  .....#
+      #..... $ $ $ $ $   $ $ $ $ $ .....#
+      #.....  $ $ $ $ $ $ $ $ $ $  .....#
+      #..... $ $ $ $ $   $ $ $ $ $ .....#
+      #.....  $ $ $ $ $ $ $ $ $ $  .....#
+      #..... $ $ $ $ $   $ $ $ $ $ .....#
+      #.....  $ $ $ $ $ $ $ $ $ $  .....#
+      #..... $ $ $ $ $   $ $ $ $ $ .....#
+      #.....  $ $ $ $ $ $ $ $ $ $  .....#
+      ###################################
+    ");
+
+    static readonly SokoField FieldDiamond = new SokoField(@"
+               #################         
+              ##               ##        
+             ##  * *$* *$*$*$*  ##       
+            ##  *$*$. *..$.$. *  ##      
+           ##  *$.$.$* *$.$.$*$*  ##     
+          ##  *$.$.$.$* * . . . .  ##    
+         ##  *$.$.$.$. * * * .$. *  ##   
+        ##  * .*.$. . * * .$# #*# *$ ##  
+       ##  *$*$. * *$* * * * * * * .$ ## 
+      ##  $$. *$.$. .$*** * # * * * .$ ##
+      #  $$. # . * .$* * *$* * *$# * *  #
+      # #.$ ##. *.* . .$# . ### .## .$  #
+      #  ##  * *$.$* * * . *$.$* * . .$ #
+      ## $.#  .$. * + *##$. * * . .$*  ##
+       ##  *. $.$. .$. # .$.$. .$.$$. ## 
+        ##  **$**$.$.$. .$.$*$***$$. ##  
+         ##  #.* *$.$*$.$.$* * * $. ##   
+          ##  * $ *$. .$.$* * * $. ##    
+           ##  *.$ *$.$.$* * * $. ##     
+            ##  *.* *$*#* * * $. ##      
+             ## .$.$.$.$.$.$.$  ##       
+              ##               ##        
+               #################         
+    ");
+
+    static readonly SokoField FieldRunner = new SokoField(@"
+      ##################################################
+      #                                                #
+      # $######################################## #### #
+      #                                           #  # #
+      # $###################### ################# #  # #
+      #                         # $               #  # #
+      ##### ################### #                 #  # #
+      #   # #                   ### $             #  # #
+      # $$# # ################################### #  # #
+      #   # #                           @      ## #  # #
+      # $$# # #### # #########################.   #### #
+      #   # #        ############............#  # ##   #
+      # $$# ###  # # #                       #  # ##$$ #
+      #   # # # ##                           #  # ##   #
+      # $$# # # #######                      #  # ##$$ #
+      #   # # # #    #.......................#  # ##   #
+      # $$# # # #    #.......................#  # ##$$ #
+      #   # # # #    #########################  # ##   #
+      # $$# # # #                            #  # ##$$ #
+      #   # # # ##############################  # ##   #
+      # $$# # #                                 # ##$$ #
+      #   # # # ###############################    # # #
+      # $$# # #                                 #    # #
+      #   # # ###################################### # #
+      #     #                                      # # #
+      # $#  #                                      # # #
+      #  # ######################################### # #
+      # $# #                                       $ # #
+      #  #                                         #   #
+      # $# #########################################$# #
+      #  #                                             #
+      ## #  ######################################## ###
+       # ######################################### $ $ #
+       # $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $   #
+       #             #                               ###
+       ###############################################  
+    ");
+    #endregion
+
+    /// <summary>
+    /// Kostruktor
+    /// </summary>
+    public FormDebugger()
+    {
+      InitializeComponent();
+      network = new RoomNetwork(FieldTest1);       // sehr einfaches Testlevel
+      //network = new RoomNetwork(FieldStart);       // Klassik Sokoban 1. Level
+      //network = new RoomNetwork(Field628);         // bisher nie gefundene Lösung mit 628 Moves
+      //network = new RoomNetwork(FieldMoves105022); // Spielfeld mit über 100k Moves
+      //network = new RoomNetwork(FieldMonster);     // aufwendiges Spielfeld mit viele Möglichkeiten
+      //network = new RoomNetwork(FieldDiamond);     // Diamand geformter Klumpen mit vielen Deadlock-Situationen
+      //network = new RoomNetwork(FieldRunner);      // einfach zu lösen, jedoch sehr viele Moves notwendig (rund 50k)
+      DisplayUpdate();
+    }
+
+    /// <summary>
+    /// aktualisiert die Anzeige
+    /// </summary>
+    void DisplayUpdate()
+    {
+      #region # // --- Room-Liste erneuern (falls notwendig) ---
+      if (listRooms.Items.Count != network.rooms.Length)
+      {
+        int oldSelected = listRooms.SelectedIndex;
+        listRooms.BeginUpdate();
+        listRooms.Items.Clear();
+        for (int i = 0; i < network.rooms.Length; i++)
+        {
+          listRooms.Items.Add("Room " + (i + 1) + " [" + network.rooms[i].fieldPosis.Length + "]");
+        }
+        listRooms.SelectedIndex = Math.Min(oldSelected, network.rooms.Length - 1);
+        listRooms.EndUpdate();
+      }
+      #endregion
+    }
+
+    /// <summary>
+    /// Methode für gedrückte Tasten
+    /// </summary>
+    /// <param name="sender">Objekt, welches dieses Event erzeugt hat</param>
+    /// <param name="e">Informationen über die gedrückten Tasten</param>
+    void Form_KeyDown(object sender, KeyEventArgs e)
+    {
+      switch (e.KeyCode)
+      {
+        case Keys.Escape: Close(); break;
+      }
+    }
+  }
+}
