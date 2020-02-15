11798008cdc2ec55661e541a09a6438af6c80a8f
MaxKlaxx <max@tetrisk.de>
2020-02-15 16:48:53 +0100

    rooms: RoomMerger nun als Klasse erstellt

---
+++ SokoWahn/SokoWahnLib/Rooms/RoomMerger.cs
@@ -0,0 +1,18 @@
+﻿
+namespace SokoWahnLib.Rooms
+{
+  /// <summary>
+  /// Klasse zum verschmelzen von zwei Räumen
+  /// </summary>
+  public class RoomMerger
+  {
+    /// <summary>
+    /// Konstruktor
+    /// </summary>
+    /// <param name="room1">erster Raum, welcher verscholzen werden soll</param>
+    /// <param name="room2">zweiter Raum, welcher verschmolzen werden soll</param>
+    public RoomMerger(Room room1, Room room2)
+    {
+    }
+  }
+}
